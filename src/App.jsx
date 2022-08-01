6a5145b4217e32b8c5cb2fbde2ac5c15c582eba4
unknown <humbertorivero9@gmail.com>
2022-07-31 21:26:42 -0300

    Update template

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ function App() {
 	return (
 		<div className="App">
       <h1>Lista de tareas</h1>
-      {
-        // Para poder utilizar el contexto dentro de nuestros
-        // componentes, necesitamos que los mismos se encuentren
-        //"envueltos" dentro del provider.
-      }
       <NuevoTodo />
       <Todos />
     </div>
